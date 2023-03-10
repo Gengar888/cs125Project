38c9bb14bdcb17617cc7542ef1a33e1cceddd2fe
Raymond Siebert <127427302+Gengar888@users.noreply.github.com>
2023-03-09 17:49:44 -0700

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define COLS 20
 
 int points = 0;
-int price100 =100;
+int price100 = 100;
 int price200 = 200;
 int price300 = 300;
 int price400 = 400;
@@ -17,19 +17,37 @@ int price400 = 400;
 
 
 void HistoryQuestions(){
+   int price;
+    char InpAns;
     priceselectionscreen();
-    
+    scanf("%d",&price);
+    if(price==price100){
+        printf(" Q1");
+    }
+    else if(price==price200){
+        printf(" Q2");
+    }   
+    else if(price==price300){
+        printf(" Q3");
+    }
+        else if(price==price400){
+        printf(" Q4");
+    }
+    else{
+        printf(" Wrong");
+    }
+
 }
 void AnimalsQuestions(){
-    
+    priceselectionscreen();
 }
     
 void GeographyQuestions(){
-    
+    priceselectionscreen();
 }
 
 void TelevisionQuestions(){
-    
+    priceselectionscreen();
 }
 
 void categories(char catstring[]){
@@ -70,15 +88,6 @@ void changeColor(int color){
   }
 }
 
-int stringcompare(char QuestionAnswer[],char InputAnswer[]){
-
-    if(strcmp(QuestionAnswer, InputAnswer)==0)
-        return 1;
-    else
-        return 0;
-
-}
-
 void print_array(char lines[ROWS][COLS]){
 
   int x;
@@ -113,37 +122,44 @@ void catselectionScreen(){
    
 }
    
-   
 void priceselectionscreen(){
-    
+    int price100 = 100;
+    int price200 = 200;
+    int price300 = 300;
+    int price400 = 400;
 
-   char price[ROWS][COLS]={
-    "***************",
-    "*      %d     *",
-    "*      %d     *",
-    "*             *",
-    "*  JEOPARDY!  *",
-    "*      %d     *",
-    "*      %d     *",
-    "*             *",
-    "***************",};
-  int y;
-  clearScreen();
-  print_array(price);
-  for(y=1; y< (ROWS-1); y++){
+    char price[ROWS][COLS]={
+            "***************",
+            "*      %d  *",
+            "*      %d  *",
+            "*             *",
+            "*  JEOPARDY!  *",
+            "*      %d  *",
+            "*      %d  *",
+            "*             *",
+            "***************"};
+    sprintf((char*)&price[1],"*    %d    *",price100);
+    sprintf((char*)&price[2],"*    %d    *",price200);
+    sprintf((char*)&price[5],"*    %d    *",price300);
+    sprintf((char*)&price[6],"*    %d    *",price400);
+
+    int y;
     clearScreen();
     print_array(price);
-    changeColor((y%3)+1);
-    
+    for(y=1; y< (ROWS-1); y++){
+        clearScreen();
+        print_array(price);
+        changeColor((y%3)+1);
+
     }
-    printf("Choose a price!");
+    printf("Choose a price! ");
 }
 
 int main(){
     catselectionScreen();
     char catstring[50];
     printf("PICK A CATEGORY!");
-    fgets(catstring, 50,stdin);
+    fgets(catstring, 50 ,stdin);
     categories(catstring);
    
     
