ac30ab490817a9d1e7670f252549eb162523f44a
Raymond Siebert <127427302+Gengar888@users.noreply.github.com>
2023-03-26 16:10:34 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+//Make I/O File to output high scores at the end
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,56 +10,150 @@
 #define COLS 20
 
 int points = 0;
-int price100 =100;
+char InpAns[50];
+int AnimalsToken100 = 0;
+int GeographyToken100 = 0;
+int HistoryToken100 = 0;
+int Television100 = 0;
+
+int AnimalsToken200 = 0;
+int GeographyToken200 = 0;
+int HistoryToken200 = 0;
+int Television200 = 0;
+
+int AnimalsToken300 = 0;
+int GeographyToken300 = 0;
+int HistoryToken300 = 0;
+int Television300 = 0;
+
+int AnimalsToken400 = 0;
+int GeographyToken400 = 0;
+int HistoryToken400 = 0;
+int Television400 = 0;
+
+int price100 = 100;
 int price200 = 200;
 int price300 = 300;
 int price400 = 400;
 
-void priceselectionscreen(){
-    
 
-   char price[ROWS][COLS]={
-    "***************",
-    "*      %d     *",
-    "*      %d     *",
-    "*             *",
-    "*  JEOPARDY!  *",
-    "*      %d     *",
-    "*      %d     *",
-    "*             *",
-    "***************",};
-  int y;
-  clearScreen();
-  print_array(price);
-  for(y=1; y< (ROWS-1); y++){
-    clearScreen();
-    print_array(price);
-    changeColor((y%3)+1);
-    
-    }
-    printf("Choose a price!");
+
+void changeColor(int color){
+  if(color==1){
+    printf(" \e[1;31m");  //bold red font
+  }
+  else if(color==2){
+    printf(" \e[1;34m");  //bold blue font
+  }
+  else if(color==3){
+    printf(" \e[1;32m");  //bold green font
+  }
+  else{
+    printf("\e[0m\n");
+  }
 }
 
+void print_array(char lines[ROWS][COLS]){
+
+  int x;
+  for (x=0; x< ROWS; x++){
+    printf("%s\n", lines[x]);
+  }
+}
+
+void clearScreen(){
+  sleep(0.1);
+  printf("\e[2J\e[H");
+}
+
+void priceselectionscreen(){
+    int price100 = 100;
+    int price200 = 200;
+    int price300 = 300;
+    int price400 = 400;
+
+    char price[ROWS][COLS]={
+            "***************",
+            "*      %d  *",
+            "*      %d  *",
+            "*             *",
+            "*  JEOPARDY!  *",
+            "*      %d  *",
+            "*      %d  *",
+            "*             *",
+            "***************"};
+    sprintf((char*)&price[1],"*    %d    *",price100);
+    sprintf((char*)&price[2],"*    %d    *",price200);
+    sprintf((char*)&price[5],"*    %d    *",price300);
+    sprintf((char*)&price[6],"*    %d    *",price400);
+
+    int y;
+    clearScreen();
+    print_array(price);
+    for(y=1; y< (ROWS-1); y++){
+        clearScreen();
+        print_array(price);
+        changeColor((y%3)+1);
+
+    }
+    printf("Choose a price! ");
+}
+
+void GeographyQuestions(){
+    int price;
+    
+    priceselectionscreen();
+    
+    scanf("%d%*c",&price); // "%*c" This makes the fgets work because it makes fgets read a character and not save it  // meaning when we hit "Enter" after picking the price, the fgets will now work.
+    if(price==price100 && GeographyToken100 == 0 ){   //Token makes sure you havent chosen this problem before
+        GeographyToken100 + 1;
+        printf("This is the smallest state in the United States. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "Rhode Island\n")==0 )
+            printf("Correct! +50 points!\n");
+            points + 50;
+            main();
+        else
+        printf("Incorrect!");
+        sleep(3);
+        main();
+        
+    }
+    else if(price==price200 && GeographyToken200 == 0){
+        printf("This is the smallest country in the world. \n");
+        fgets(InpAns, 50 ,stdin);
+    }   
+    else if(price==price300 && GeographyToken300 == 0){
+            printf("This capital city is the most populous in the Western Hemisphere. \n");
+            fgets(InpAns, 50 ,stdin);
+    }
+    else if(price==price400){
+            printf("This sea is the only sea on the planet to not have a coastline. \n");
+            fgets(InpAns, 50 ,stdin);
+        }
+    else{
+        printf(" Wrong");
+    }
+}
 
 
 void HistoryQuestions(){
     int price;
-    char InpAns;
     priceselectionscreen();
-    scanf("%d",&price);
+    
+    scanf("%d%*c",&price);
     if(price==price100){
-        printf(" Q1");
-        
-        
+        printf("Q1 ");
+        fgets(InpAns, 50 ,stdin);
     }
     else if(price==price200){
-        printf(" Q2");
+        printf("Q2 ");
     }   
     else if(price==price300){
-        printf(" Q3");
+        printf("Q3 ");
     }
         else if(price==price400){
-        printf(" Q4");
+        printf("Q4 ");
     }
     else{
         printf(" Wrong");
@@ -65,15 +161,16 @@ void HistoryQuestions(){
 
 }
 void AnimalsQuestions(){
+    int price;
+    
+    priceselectionscreen();
     
 }
     
-void GeographyQuestions(){
-    
-}
+
 
 void TelevisionQuestions(){
-    
+    priceselectionscreen();
 }
 
 void categories(char catstring[]){
@@ -94,43 +191,6 @@ void categories(char catstring[]){
     }
 }
 
-void clearScreen(){
-  sleep(0.1);
-  printf("\e[2J\e[H");
-}
-
-void changeColor(int color){
-  if(color==1){
-    printf(" \e[1;31m");  //bold red font
-  }
-  else if(color==2){
-    printf(" \e[1;34m");  //bold blue font
-  }
-  else if(color==3){
-    printf(" \e[1;32m");  //bold green font
-  }
-  else{
-    printf("\e[0m\n");
-  }
-}
-
-int stringcompare(char QuestionAnswer[],char InputAnswer[]){
-
-    if(strcmp(QuestionAnswer, InputAnswer)==0)
-        return 1;
-    else
-        return 0;
-
-}
-
-void print_array(char lines[ROWS][COLS]){
-
-  int x;
-  for (x=0; x< ROWS; x++){
-    printf("%s\n", lines[x]);
-  }
-
-}
 
 
 void catselectionScreen(){
@@ -156,12 +216,12 @@ void catselectionScreen(){
   }
    
 }
-   
+
 int main(){
     catselectionScreen();
     char catstring[50];
     printf("PICK A CATEGORY!");
-    fgets(catstring, 50,stdin);
+    fgets(catstring, 50 ,stdin);
     categories(catstring);
    
     
