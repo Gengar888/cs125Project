1e3f765178501c4dd6d536649f0c9fefa9213b46
THEOFELD2000 <127454999+THEOFELD2000@users.noreply.github.com>
2023-03-26 17:34:07 -0700

    Update main.c

--- main.c
+++ main.c
@@ -10,26 +10,26 @@
 #define COLS 20
 
 int points = 0;
-char InpAns[50];
+char InpAns[100];
 int AnimalsToken100 = 0;
 int GeographyToken100 = 0;
 int HistoryToken100 = 0;
-int Television100 = 0;
+int TelevisionToken100 = 0;
 
 int AnimalsToken200 = 0;
 int GeographyToken200 = 0;
 int HistoryToken200 = 0;
-int Television200 = 0;
+int TelevisionToken200 = 0;
 
 int AnimalsToken300 = 0;
 int GeographyToken300 = 0;
 int HistoryToken300 = 0;
-int Television300 = 0;
+int TelevisionToken300 = 0;
 
 int AnimalsToken400 = 0;
 int GeographyToken400 = 0;
 int HistoryToken400 = 0;
-int Television400 = 0;
+int TelevisionToken400 = 0;
 
 int price100 = 100;
 int price200 = 200;
@@ -207,7 +207,7 @@ int HistoryQuestions(){
     else if(price==price200 && HistoryToken200 == 0){
         HistoryToken200++;
         printf("This Japanese city became the first in history to have a nuclear warhead dropped on it? \n");
-        fgets(InpAns, 50 ,stdin);
+        fgets(InpAns, 100 ,stdin);
         if (strcmp (InpAns, "What is Hiroshima?\n")==0 ){
             printf("Correct! +200 points!\n");
             while( getchar() != '\n' );
@@ -242,13 +242,13 @@ int HistoryQuestions(){
         HistoryToken400++;
         printf("This king lead the largest calvary charge in recorded history. \n");
         fgets(InpAns, 50 ,stdin);
-        if (strcmp (InpAns, "Who is King John III Sobieski?")==0 ){
+        if (strcmp (InpAns, "Who is King John III Sobieski?\n")==0 ){
             printf("Correct! +400 points!\n");
             while( getchar() != '\n' );
             points + 400;
             main();
         }
-        else if (strcmp (InpAns, "Who is King John 3 Sobieski?")==0 ){
+        else if (strcmp (InpAns, "Who is King John 3 Sobieski?\n")==0 ){
             printf("Correct! +400 points!\n");
             while( getchar() != '\n' );
             points + 400;
@@ -273,13 +273,163 @@ int AnimalsQuestions(){
     
     priceselectionscreen();
     
+    scanf("%d%*c",&price); // "%*c" This makes the fgets work because it makes fgets read a character and not save it  // meaning when we hit "Enter" after picking the price, the fgets will now work.
+    if(price==price100 && AnimalsToken100 == 0 ){   //Token makes sure you havent chosen this problem before
+        AnimalsToken100++;
+        printf("This is animal reaches the fastest speeds on land. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "What is a cheetah?\n")==0 ){
+            printf("Correct! +100 points!\n");
+            while( getchar() != '\n' );
+            points + 100;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+        }
 }
-    
+    else if(price==price200 && AnimalsToken200 == 0){
+        AnimalsToken200++;
+        printf("This is the only member of the genus equus to have stripes. \n");
+        fgets(InpAns, 100 ,stdin);
+        if (strcmp (InpAns, "What is a zebra?\n")==0 ){
+            printf("Correct! +200 points!\n");
+            while( getchar() != '\n' );
+            points + 200;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+    }
+}
+    else if(price==price300 && AnimalsToken300 == 0){
+        AnimalsToken300++;
+        printf("This animal has the longest neck of any animal. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "What is a giraffe?\n")==0 ){
+            printf("Correct! +300 points!\n");
+            while( getchar() != '\n' );
+            points + 300;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+    }
+}
+    else if(price==price400 && AnimalsToken400 == 0){
+        AnimalsToken400++;
+        printf("This animal reaches the fastest speeds in the entire animal kingdom. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "What is a peregrine falcon?\n")==0 ){
+            printf("Correct! +400 points!\n");
+            while( getchar() != '\n' );
+            points + 400;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+    }
+}
+    else{
+        printf("You have already chosen this price & category!");
+        while( getchar() != '\n' );
+        main();
+    }
+} 
 
 
 int TelevisionQuestions(){
+    int price;
+    
     priceselectionscreen();
+    
+    scanf("%d%*c",&price); // "%*c" This makes the fgets work because it makes fgets read a character and not save it  // meaning when we hit "Enter" after picking the price, the fgets will now work.
+    if(price==price100 && TelevisionToken100 == 0 ){   //Token makes sure you havent chosen this problem before
+        TelevisionToken100++;
+        printf("This is the name of the Matrix's main character. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "Who is Neo?\n")==0 ){
+            printf("Correct! +100 points!\n");
+            while( getchar() != '\n' );
+            points + 100;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+        }
 }
+    else if(price==price200 && TelevisionToken200 == 0){
+        TelevisionToken200++;
+        printf("This movie won Best Picture in 2020. \n");
+        fgets(InpAns, 100 ,stdin);
+        if (strcmp (InpAns, "What is Parasite?\n")==0 ){
+            printf("Correct! +200 points!\n");
+            while( getchar() != '\n' );
+            points + 200;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+    }
+}
+    else if(price==price300 && TelevisionToken300 == 0){
+        TelevisionToken300++;
+        printf("This is the first movie directed by Quentin Tarantino. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "What is Reservoir Dogs?\n")==0 ){
+            printf("Correct! +300 points!\n");
+            while( getchar() != '\n' );
+            points + 300;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+    }
+}
+    else if(price==price400 && TelevisionToken400 == 0){
+        TelevisionToken400++;
+        printf("This is the total number of Simpson episodes. \n");
+        fgets(InpAns, 50 ,stdin);
+        if (strcmp (InpAns, "What is 744?\n")==0 ){
+            printf("Correct! +400 points!\n");
+            while( getchar() != '\n' );
+            points + 400;
+            main();
+        }
+        
+        else{
+        printf("Incorrect!");
+        while( getchar() != '\n' );
+        main();
+    }
+}
+    else{
+        printf("You have already chosen this price & category!");
+        while( getchar() != '\n' );
+        main();
+    }
+} 
 
 void categories(char catstring[]){
     if (strcmp(catstring, "History\n")==0)
@@ -328,7 +478,7 @@ void catselectionScreen(){
 int main(){
     catselectionScreen();
     char catstring[50];
-    printf("PICK A CATEGORY!");
+    printf("PICK A CATEGORY! ");
     fgets(catstring, 50 ,stdin);
     categories(catstring);
    
