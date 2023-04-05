a3dcf0298c6e3e2d742cb88e1370c791812fc71c
THEOFELD2000 <127454999+THEOFELD2000@users.noreply.github.com>
2023-04-04 17:04:14 -0700

    Update screen.h

--- Working Project/screen.h
+++ Working Project/screen.h
@@ -1,5 +1,9 @@
+//Authors: Raymond Siebert, Theodore Feldman, & Ruslan Gabdulin
+
 #pragma once
 #include "constants.h"
 void changeColor(int color);
 void print_array(char lines[ROWS][COLS]);
 void clearScreen();
+
+//George helped with the .h file creation
