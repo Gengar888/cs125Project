a5cec0445c398f1f6f8a2ac893a7c238e9f4bf83
THEOFELD2000 <127454999+THEOFELD2000@users.noreply.github.com>
2023-04-04 17:03:58 -0700

    Update constants.h

--- Working Project/constants.h
+++ Working Project/constants.h
@@ -1,4 +1,8 @@
+//Authors: Raymond Siebert, Theodore Feldman, & Ruslan Gabdulin
+
 #pragma once
 #include "catstringselection.h"
 #define ROWS 9
 #define COLS 20
+
+//George helped with the .h file creation
